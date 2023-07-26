3012b3b0568d97a28f8703d13d1bb95c810061ee
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-07-26 12:13:34 +0300

    created class Letter, Response

---
+++ src/main/java/by/alex/proclamation/persistence/model/Letter.java
@@ -0,0 +1,32 @@
+package by.alex.proclamation.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "letter")
+public class Letter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "prof")
+    private String prof;
+
+}
+
---
+++ src/main/java/by/alex/proclamation/persistence/model/Response.java
@@ -0,0 +1,9 @@
+package by.alex.proclamation.persistence.model;
+
+import lombok.Data;
+
+@Data
+public class Response {
+    private int responseCod;
+    private String responseText;
+}
