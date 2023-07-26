c0f0b69336d1c5c1c57843ed18f4a59b92f889cd
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-07-26 21:33:52 +0300

    changed controllers

--- src/main/java/by/alex/proclamation/controller/HomeController.java
+++ src/main/java/by/alex/proclamation/controller/HomeController.java
@@ -1,21 +1,19 @@
 package by.alex.proclamation.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class HomeController {
 
 
     @RequestMapping(value = "/home")
     public ModelAndView home() {
-
+        log.info("Старт приложения");
         return new ModelAndView("index");
     }
 
--- src/main/java/by/alex/proclamation/controller/MailController.java
+++ src/main/java/by/alex/proclamation/controller/MailController.java
@@ -1,36 +1,32 @@
 package by.alex.proclamation.controller;
 
-import by.alex.proclamation.model.Letter;
+import com.google.gson.Gson;
+import by.alex.proclamation.persistence.dto.LetterDto;
+import by.alex.proclamation.service.LetterService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/mail")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
 public class MailController {
+    private final LetterService letServ;
+    private final Gson gson;
 
-    @PostMapping(value = "/send", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public String sendLetter(@RequestBody Letter letter){
-
-
-        return "index";
+    @PostMapping(value = "/send", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public String sendLetter(@Valid @RequestBody LetterDto letDto) {
+     return    gson.toJson(letServ.createApplication(letDto));
 
     }
-//
-//        AuthorDto authorDto = new AuthorDto();
-//        boolean isAdd = author.getId() == null;
-//        try {
-//            authorDto = service.save(author);
-//        } catch (Exception e) {
-//            logger.warn("Created author {} is not successful", author, e);
-//        }
-//        if (isAdd) {
-//            model = new ModelAndView("redirect:/authors/" + authorDto.getId());
-//
-//        } else {
-//            model = new ModelAndView("redirect:/authors");
-//        }
-//
-//    }
 
 }
