2778e4ec2e9c7c46d09018776638170e0c1e02f5
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-07-26 21:33:07 +0300

    changed html and js

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,11 +1,12 @@
 function applyHandler(formElement) {
    const data = $(formElement).serializeArray();
-   console.log(formElement,data)
    const obj={};
    for (const field of data){
       obj[field.name]= field.value;
    }
    const $btn = $(formElement).find(".btn-primary").button('loading');
+   $(formElement).find('.form-group').removeClass("has-error");
+   $(formElement).find('.form-group .help-block').text('');
    $.ajax({
       url: '/mail/send',
       type: 'POST',
@@ -13,11 +14,22 @@ function applyHandler(formElement) {
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
       success: function (msg) {
+
          if (msg.statusCode === 400){
-            $.growl.error({title:"Ошибка" ,message: "Проверьте введенные даные" });
-         }else {
+            $.growl.error({title:"Ошибка" ,message: "Проверьте введенные данные" });
+               console.log(msg.violations)
+            msg.violations.forEach(function (vio){
+               $(formElement).find(`input[name="${vio.fieldName}"]`).parent().addClass("has-error");
+               $(formElement).find(`input[name="${vio.fieldName}"]`).next(".help-block").text(vio.message)
+            });
+         }
+         else if(msg.statusCode===303){
+            $('.modal').modal('hide');
+            $.growl.error({title:"Неудача" ,message: msg.message });
+         }
+         else {
          $('.modal').modal('hide');
-            $.growl.notice({title:"Успех" ,message: msg });
+            $.growl.notice({title:"Успех" ,message: msg.message });
          }
       },
       complete: function (){
