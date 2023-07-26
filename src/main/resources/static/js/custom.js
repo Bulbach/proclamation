c6a4cd7e5cfd690b4bcc3d6f0ed974cd6ac77004
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-07-26 12:07:52 +0300

    change front part(add library, main page ...)

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,17 +1,30 @@
 function applyHandler(formElement) {
    const data = $(formElement).serializeArray();
    console.log(formElement,data)
+   const obj={};
+   for (const field of data){
+      obj[field.name]= field.value;
+   }
+   const $btn = $(formElement).find(".btn-primary").button('loading');
    $.ajax({
-      url: 'Ajax.ashx',
+      url: '/mail/send',
       type: 'POST',
-      data: JSON.stringify(data),
+      data: JSON.stringify(obj),
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
       success: function (msg) {
-         alert(msg);
+         if (msg.statusCode === 400){
+            $.growl.error({title:"Ошибка" ,message: "Проверьте введенные даные" });
+         }else {
+         $('.modal').modal('hide');
+            $.growl.notice({title:"Успех" ,message: msg });
+         }
       },
-      error: function () {
-         alert("Something wrong!");
+      complete: function (){
+         $btn.button('reset')
+      },
+      error: function (msg) {
+         $.growl.error({title:"Неудача" ,message: "Что-то пошло не так! Попробуйте ещё раз позже" });
       }
          });
    return false;
