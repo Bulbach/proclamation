5b4dcf8a1799327ff8f26c04cbec586f5ffcf59c
Alex verezubov <60190100+Bulbach@users.noreply.github.com>
2023-07-26 21:35:44 +0300

    changed pattern validate

--- src/main/java/by/alex/proclamation/persistence/dto/LetterDto.java
+++ src/main/java/by/alex/proclamation/persistence/dto/LetterDto.java
@@ -3,6 +3,7 @@ package by.alex.proclamation.persistence.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -11,6 +12,7 @@ import javax.validation.constraints.Size;
 @Data
 @NoArgsConstructor
 @Component
+@Validated
 public class LetterDto {
     private Long id;
     @NotBlank
@@ -18,7 +20,7 @@ public class LetterDto {
     private String name;
     @NotBlank
     @Size(max = 25)
-    @Pattern(regexp = "\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*\\.\\w{2,4}")
+    @Pattern(regexp = "\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*\\.\\w{2,4}", message = "Не соответствует формату email адреса")
     private String email;
     @NotBlank
     @Size(max = 15)
